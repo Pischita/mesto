1009b9c2b290add6519ae466beec1fc48a960da3
Ivan Pischita <aaib@tut.by>
2020-12-09 22:40:07 +0300

    feat: Сделать добавление карточки

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
-let popup = document.querySelector('.popup');
+const editPopup = document.querySelector('.popup_name_edit');
+const addPopup = document.querySelector('.popup_name_add');
+
 let editBtn = document.querySelector('.profile__edit-button');
-let closePopupBtn = document.querySelector('.popup__close-button');
+const addBtn = document.querySelector('.profile__add-button');
+let closePopupBtn = document.querySelectorAll('.popup__close-button');
 let editForm = document.querySelector('.popup__container');
 let nameNode = document.querySelector('.profile__name');
 let nameInput = document.querySelector('.popup__field_name_name');
@@ -8,25 +11,92 @@ let nameInput = document.querySelector('.popup__field_name_name');
 let positionNode = document.querySelector('.profile__position');
 let positionInput = document.querySelector('.popup__field_name_position');
 
+const initialCards = [{
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
 
-function showPopup(event){
+
+function showEditPopup(event) {
     nameInput.value = nameNode.textContent;
     positionInput.value = positionNode.textContent;
-    popup.classList.add('popup_opened');
+    editPopup.classList.add('popup_opened');
 }
 
-function closePopup(event){
-    popup.classList.remove('popup_opened');
+function showAddPopup() {
+    addPopup.classList.add('popup_opened');
 }
 
-function savePopup(event){
+function closePopup(event) {
+    event.target.closest('.popup').classList.remove('popup_opened');
+}
+
+function saveEditPopup(event) {
     event.preventDefault();
-    nameNode.textContent = nameInput.value;
-    positionNode.textContent = positionInput.value;
-    closePopup();
+
+    if (event.target === document.forms.edit) {
+        nameNode.textContent = nameInput.value;
+        positionNode.textContent = positionInput.value;
+    } else if (event.target === document.forms.add) {
+        const name = event.target.querySelector('.popup__field_name_name').value;
+        const link = event.target.querySelector('.popup__field_name_source-image').value;
+        addElement({
+            name: name,
+            link: link
+        });
+
+    }
+
+    closePopup(event);
 }
 
+function addElement(el) {
+    const mestoTemplate = document.querySelector('#mesto-element');
+    const container = document.querySelector('.elements');
 
-editBtn.addEventListener('click', showPopup);
-closePopupBtn.addEventListener('click', closePopup);
-editForm.addEventListener('submit', savePopup);
+    const mestoNode = mestoTemplate.content.cloneNode(true);
+    const imgNode = mestoNode.querySelector('.element__image');
+    imgNode.src = el.link;
+    imgNode.alt = el.name;
+    mestoNode.querySelector('.element__name').textContent = el.name;
+    container.append(mestoNode);
+}
+
+function initializePage() {
+    initialCards.forEach(item => {
+        addElement(item);
+    });
+}
+
+editBtn.addEventListener('click', showEditPopup);
+closePopupBtn.forEach(btn => {
+    btn.addEventListener('click', closePopup);
+});
+
+document.querySelectorAll('.popup__container').forEach(form => {
+    form.addEventListener('submit', saveEditPopup);
+});
+
+addBtn.addEventListener('click', showAddPopup);
+initializePage();
